c3ccc1ae1d9a8f3b01c9b0362d56093c85774799
Tran Khac Binh Duong <tran.khac.binh.duong@azoom.jp>
2025-01-20 14:12:03 +0700

    :thumbsup: Add about and home pages

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about-page">
+    <h1>This is an about page</h1>
+    <p>Go to the <NuxtLink to="/">Home</NuxtLink> page</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  margin-top: 20px;
+}
+
+h1 {
+  color: #41b883;
+}
+
+p {
+  color: #35495e;
+}
+
+a {
+  color: #41b883;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+    <h1>Welcome to the home page</h1>
+    <p>Go to the <NuxtLink to="/about">About</NuxtLink> page</p>
+  </div>
+</template>
+<style scoped>
+.home-page {
+  text-align: center;
+  margin-top: 20px;
+}
+
+h1 {
+  color: #35495e;
+}
+
+p {
+  color: #41b883;
+}
+
+a {
+  color: #35495e;
+}
+</style>
